359282d6e8312b1002d7bf4a747d056d58c06872
2k8hahadaylatao <97292950+2k8hahadaylatao@users.noreply.github.com>
2022-01-15 06:14:03 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function go() {
+  console.log("Lên đầu trang ");
+  window.location.replace("#picstop");
+}
+//đầu trang
+function go2() {
+  console.log("VÔ TRANG ĐĂNG NHẬP");
+  window.location.replace("https://tbq86.csb.app/inlog.html");
+}
+//đăng nhập
